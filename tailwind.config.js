e42581342d6caa4d60505bed6a927683e3f872e7
Vishal21121 <vs84077000@gmail.com>
2024-12-15 13:18:28 +0530

    made necessary changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -7,5 +9,5 @@ export default {
   daisyui: {
     themes: ["night"],
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
